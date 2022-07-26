9761b0c729adae4226573fc035a193dd53dfc627
Ruben <rubsuadav@alum.us.es>
2022-07-26 20:00:44 +0200

    Entidad echa

--- src/main/java/acme/entities/pimpams/Pimpam.java
+++ src/main/java/acme/entities/pimpams/Pimpam.java
@@ -1,66 +1,62 @@
-package acme.entities.items;
+package acme.entities.pimpams;
+
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.pimpams.Pimpam;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
-import acme.roles.Chef;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Item extends AbstractEntity{
+public class Pimpam extends AbstractEntity {
 
 	protected static final long	serialVersionUID= 1L;
 
-	@NotNull
-	protected Type typeEntity; 
-
-	@NotBlank
-	@Length (min=1, max=100)
-	protected String name;
-
 	@Column(unique = true)
-	@Pattern(regexp = "^([A-Z]{2}:)?[A-Z]{3}-[0-9]{3}$")
+	@Pattern(regexp = "^\\d{2}-\\d{6}$")
 	@NotBlank
 	protected String code;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date instantiationMoment;
+
+	@NotBlank
+	@Length (min=1, max=100)
+	protected String title;
+
 	@NotBlank
 	@Length (min=1, max=255)
-	protected String description;
+	protected String desciption;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date initial;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date end;
 
 	@Valid
 	@NotNull
-	protected Money retailPrice;
+	protected Money budget;
 
 	@URL
 	protected String link;
-	
-	protected boolean published;
-	
-	@ManyToOne(optional = false)
-	@Valid
-	@NotNull
-	protected Chef chef;
-	
-	// CC
-	
-	@OneToOne(optional = false)
-	@Valid
-	@NotNull
-	protected Pimpam pimpam;
-	
 }
